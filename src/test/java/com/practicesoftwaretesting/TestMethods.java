b0c1c209ab75cab28e5ac05e15047e9b45807b20
RomanSibakovskij <j4023958@gmail.com>
2024-11-18 11:23:37 +0200

    Add empty TestMethods sheet

---
+++ src/test/java/com/practicesoftwaretesting/TestMethods.java
@@ -0,0 +1,11 @@
+package com.practicesoftwaretesting;
+
+import org.slf4j.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestMethods extends BaseTest{
+
+    protected static final Logger logger = LoggerFactory.getLogger(TestMethods.class);
+
+    
+}
