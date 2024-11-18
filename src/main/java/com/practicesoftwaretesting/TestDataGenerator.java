a35f00408b79e30253d708b6e47033507fd85458
RomanSibakovskij <j4023958@gmail.com>
2024-11-18 18:00:11 +0200

    Add empty TestDataGenerator sheet

---
+++ src/main/java/com/practicesoftwaretesting/TestDataGenerator.java
@@ -0,0 +1,9 @@
+package com.practicesoftwaretesting;
+
+import org.openqa.selenium.*;
+
+public class TestDataGenerator extends BasePage{
+
+
+    public TestDataGenerator(WebDriver driver) {super(driver);}
+}
