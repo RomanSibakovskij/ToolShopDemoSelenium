5ee7257c6508bfd78d77c7860e4d9c47c9e3d30b
RomanSibakovskij <j4023958@gmail.com>
2024-11-19 15:31:21 +0200

    Add empty SignUpInvalidSingularInputPage utility class sheet

---
+++ src/main/java/com/practicesoftwaretesting/SignUpInvalidSingularInputPage.java
@@ -0,0 +1,9 @@
+package com.practicesoftwaretesting;
+
+import org.openqa.selenium.*;
+
+public class SignUpInvalidSingularInputPage extends BasePage{
+
+    public SignUpInvalidSingularInputPage(WebDriver driver) {super(driver);}
+
+}
