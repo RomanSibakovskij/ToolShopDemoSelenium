5417f16b95cef55ea1db5009d6f347d517db0aa3
RomanSibakovskij <j4023958@gmail.com>
2024-11-19 16:47:53 +0200

    Add empty SignUpPageTooLongSingularInputPage utility class sheet

---
+++ src/main/java/com/practicesoftwaretesting/SignUpPageTooLongSingularInputPage.java
@@ -0,0 +1,11 @@
+package com.practicesoftwaretesting;
+
+import org.openqa.selenium.*;
+
+public class SignUpPageTooLongSingularInputPage extends BasePage{
+    
+
+    public SignUpPageTooLongSingularInputPage(WebDriver driver) {super(driver);}
+
+
+}
