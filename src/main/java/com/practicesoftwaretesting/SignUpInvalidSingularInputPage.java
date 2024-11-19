73cb68da856769fff2772c6aa495aa37e0d3e5b0
RomanSibakovskij <j4023958@gmail.com>
2024-11-19 15:33:23 +0200

    Add web elements for SignUpInvalidSingularInputPage

--- src/main/java/com/practicesoftwaretesting/SignUpInvalidSingularInputPage.java
+++ src/main/java/com/practicesoftwaretesting/SignUpInvalidSingularInputPage.java
@@ -1,9 +1,42 @@
 package com.practicesoftwaretesting;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
 
 public class SignUpInvalidSingularInputPage extends BasePage{
 
+    //input form elements
+    @FindBy(xpath = "//input[@id='first_name']")
+    private WebElement firstNameInputField;
+    @FindBy(xpath = "//input[@id='last_name']")
+    private WebElement lastNameInputField;
+    @FindBy(xpath = "//input[@id='address']")
+    private WebElement addressInputField;
+    @FindBy(xpath = "//input[@id='postcode']")
+    private WebElement postCodeInputField;
+    @FindBy(xpath = "//input[@id='city']")
+    private WebElement cityInputField;
+    @FindBy(xpath = "//input[@id='state']")
+    private WebElement stateInputField;
+    @FindBy(xpath = "//input[@id='phone']")
+    private WebElement phoneInputField;
+    @FindBy(xpath = "//input[@id='email']")
+    private WebElement emailAddressInputField;
+    @FindBy(xpath = "//input[@id='password']")
+    private WebElement passwordInputField;
+    @FindBy(xpath = "//button[@class='btn btn-outline-secondary']")
+    private WebElement passwordViewButton;
+    @FindBy(xpath = "//div[@id='passwordHelp']")
+    private WebElement passwordInputHint;
+    @FindBy(xpath = "//div[@class='strength-labels']")
+    private WebElement passwordStrengthBar;
+    //invalid input error message elements
+    @FindBy(xpath = "//div[@class='alert alert-danger']")
+    private WebElement invalidUserInputError;
+    @FindBy(xpath = "//div[@class='alert alert-danger mt-3']")
+    private WebElement invalidInputError;
+
     public SignUpInvalidSingularInputPage(WebDriver driver) {super(driver);}
+    
 
 }
