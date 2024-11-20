f9149cfe7dade5d42dc82854723561849a983273
RomanSibakovskij <j4023958@gmail.com>
2024-11-20 11:57:43 +0200

    Add empty MyAccountPage sheet

---
+++ src/main/java/com/practicesoftwaretesting/MyAccountPage.java
@@ -0,0 +1,9 @@
+package com.practicesoftwaretesting;
+
+import org.openqa.selenium.*;
+
+public class MyAccountPage extends BasePage{
+
+    public MyAccountPage(WebDriver driver) {super(driver);}
+
+}
