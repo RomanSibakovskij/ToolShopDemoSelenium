52fdcf672b7204d380c571e89d077ea151840d65
RomanSibakovskij <j4023958@gmail.com>
2024-11-22 15:58:39 +0200

    Minor test name correction

--- src/test/java/com/practicesoftwaretesting/CheckoutProductPageTest.java
+++ src/test/java/com/practicesoftwaretesting/CheckoutProductPageTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.*;
 
 public class CheckoutProductPageTest extends TestMethods{
 
-    //Test 005 -> add bolt cutters product to cart test (as a registered user)
+    //Test 005 -> add bolt cutters product to cart test (as a registered user) // (due to fatal bug (user logout after 20-30 seconds with inability to log back in with valid credentials), further testng is aborted here until the webpage is fixed)
     @Test
-    @DisplayName("Add Bolt Cutters To Cart Test (as a registered user)")
+    @DisplayName("Add Bolt Cutters To Cart Test")
     @Tag("Add_Singular_Product_To_Cart_Test")
     void addBoltCuttersToShoppingCartTest(){
         HomePage homePage = new HomePage(driver);
@@ -20,7 +20,7 @@ public class CheckoutProductPageTest extends TestMethods{
         //return to homepage test
         returnToHomePageTestMethod(homePage);
         //test title (for logging)
-        System.out.println("Add Bolt Cutters To Cart Test (as a registered user)" + "\n");
+        System.out.println("Add Bolt Cutters To Cart Test" + "\n");
         //add 'Bolt cutters' to cart test
         addBoltCuttersToCartTest();
     }
