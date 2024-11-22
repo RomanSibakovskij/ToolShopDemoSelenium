540fbad58f02f608ee39e6ebf7112231f4582a53
RomanSibakovskij <j4023958@gmail.com>
2024-11-22 07:05:24 +0200

    Add empty ShoppingCartPage sheet

---
+++ src/main/java/com/practicesoftwaretesting/ShoppingCartPage.java
@@ -0,0 +1,11 @@
+package com.practicesoftwaretesting;
+
+import org.openqa.selenium.*;
+
+public class ShoppingCartPage extends BasePage{
+
+    
+    public ShoppingCartPage(WebDriver driver) {super(driver);}
+
+
+}
